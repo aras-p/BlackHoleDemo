a0ec38bd3a8243af0624b731e53f13e88e8ea0fe
Aras Pranckevicius <aras@unity3d.com>
2018-08-22 19:45:52 +0300

    Add exploding particle fx into scene

---
+++ Assets/Scripts/RuntimeTimelineSeek.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class RuntimeTimelineSeek : MonoBehaviour
+{
+    public float delta = 5.0f;
+    PlayableDirector _director;
+
+	void Start ()
+    {
+        _director = GetComponent<PlayableDirector>();
+	}
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+            _director.time = _director.time - delta;
+        if (Input.GetKeyDown(KeyCode.P))
+            _director.time = _director.time + delta;
+	}
+}
