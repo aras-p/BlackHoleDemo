30e34803a8a355b16a46a7c310d02699b68db8a9
Aras Pranckevicius <aras@unity3d.com>
2018-08-23 10:52:40 +0300

    Add Keijiro's AudioSpectrum, and changed it to feed data into shaders

---
+++ Assets/External/Keijiro/AudioSpectrum/AudioSpectrum.cs
@@ -0,0 +1,122 @@
+﻿// Audio spectrum component
+// By Keijiro Takahashi, 2013
+// https://github.com/keijiro/unity-audio-spectrum
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class AudioSpectrum : MonoBehaviour
+{
+    #region Band type definition
+    public enum BandType {
+        FourBand,
+        FourBandVisual,
+        EightBand,
+        TenBand,
+        TwentySixBand,
+        ThirtyOneBand
+    };
+
+    static float[][] middleFrequenciesForBands = {
+        new float[]{ 125.0f, 500, 1000, 2000 },
+        new float[]{ 250.0f, 400, 600, 800 },
+        new float[]{ 63.0f, 125, 500, 1000, 2000, 4000, 6000, 8000 },
+        new float[]{ 31.5f, 63, 125, 250, 500, 1000, 2000, 4000, 8000, 16000 },
+        new float[]{ 25.0f, 31.5f, 40, 50, 63, 80, 100, 125, 160, 200, 250, 315, 400, 500, 630, 800, 1000, 1250, 1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000 },
+        new float[]{ 20.0f, 25, 31.5f, 40, 50, 63, 80, 100, 125, 160, 200, 250, 315, 400, 500, 630, 800, 1000, 1250, 1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000, 10000, 12500, 16000, 20000 },
+    };
+    static float[] bandwidthForBands = {
+        1.414f, // 2^(1/2)
+        1.260f, // 2^(1/3)
+        1.414f, // 2^(1/2)
+        1.414f, // 2^(1/2)
+        1.122f, // 2^(1/6)
+        1.122f  // 2^(1/6)
+    };
+    #endregion
+
+    #region Public variables
+    public int numberOfSamples = 1024;
+    public BandType bandType = BandType.TenBand;
+    public float fallSpeed = 0.08f;
+    public float sensibility = 8.0f;
+    #endregion
+
+    #region Private variables
+    float[] rawSpectrum;
+    float[] levels;
+    float[] peakLevels;
+    float[] meanLevels;
+    #endregion
+
+    #region Public property
+    public float[] Levels {
+        get { return levels; }
+    }
+
+    public float[] PeakLevels {
+        get { return peakLevels; }
+    }
+    
+    public float[] MeanLevels {
+        get { return meanLevels; }
+    }
+    #endregion
+
+    #region Private functions
+    void CheckBuffers ()
+    {
+        if (rawSpectrum == null || rawSpectrum.Length != numberOfSamples) {
+            rawSpectrum = new float[numberOfSamples];
+        }
+        var bandCount = middleFrequenciesForBands [(int)bandType].Length;
+        if (levels == null || levels.Length != bandCount) {
+            levels = new float[bandCount];
+            peakLevels = new float[bandCount];
+            meanLevels = new float[bandCount];
+        }
+    }
+
+    int FrequencyToSpectrumIndex (float f)
+    {
+        var i = Mathf.FloorToInt (f / AudioSettings.outputSampleRate * 2.0f * rawSpectrum.Length);
+        return Mathf.Clamp (i, 0, rawSpectrum.Length - 1);
+    }
+    #endregion
+
+    #region Monobehaviour functions
+    void Awake ()
+    {
+        CheckBuffers ();
+    }
+
+    void Update ()
+    {
+        CheckBuffers ();
+
+        AudioListener.GetSpectrumData (rawSpectrum, 0, FFTWindow.BlackmanHarris);
+
+        float[] middlefrequencies = middleFrequenciesForBands [(int)bandType];
+        var bandwidth = bandwidthForBands [(int)bandType];
+
+        var falldown = fallSpeed * Time.deltaTime;
+        var filter = Mathf.Exp (-sensibility * Time.deltaTime);
+
+        for (var bi = 0; bi < levels.Length; bi++) {
+            int imin = FrequencyToSpectrumIndex (middlefrequencies [bi] / bandwidth);
+            int imax = FrequencyToSpectrumIndex (middlefrequencies [bi] * bandwidth);
+
+            var bandMax = 0.0f;
+            for (var fi = imin; fi <= imax; fi++) {
+                bandMax = Mathf.Max (bandMax, rawSpectrum [fi]);
+            }
+
+            levels [bi] = bandMax;
+            peakLevels [bi] = Mathf.Max (peakLevels [bi] - falldown, bandMax);
+            meanLevels [bi] = bandMax - (bandMax - meanLevels [bi]) * filter;
+        }
+        Shader.SetGlobalFloatArray("_GlobalAudioSpectrumLevel", levels);
+        Shader.SetGlobalFloatArray("_GlobalAudioSpectrumPeak", peakLevels);
+        Shader.SetGlobalFloatArray("_GlobalAudioSpectrumMean", meanLevels);
+    }
+    #endregion
+}
---
+++ Assets/External/Keijiro/AudioSpectrum/AudioSpectrumInspector.cs
@@ -0,0 +1,90 @@
+﻿// Audio spectrum component
+// By Keijiro Takahashi, 2013
+// https://github.com/keijiro/unity-audio-spectrum
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(AudioSpectrum))]
+public class AudioSpectrumInspector : Editor
+{
+    static string[] sampleOptionStrings = {
+        "256", "512", "1024", "2048", "4096"
+    };
+    static int[] sampleOptions = {
+        256, 512, 1024, 2048, 4096
+    };
+    static string[] bandOptionStrings = {
+        "4 band", "4 band (visual)", "8 band", "10 band (ISO standard)", "26 band", "31 band (FBQ3102)"
+    };
+    static int[] bandOptions = {
+        (int)AudioSpectrum.BandType.FourBand,
+        (int)AudioSpectrum.BandType.FourBandVisual,
+        (int)AudioSpectrum.BandType.EightBand,
+        (int)AudioSpectrum.BandType.TenBand,
+        (int)AudioSpectrum.BandType.TwentySixBand,
+        (int)AudioSpectrum.BandType.ThirtyOneBand
+    };
+
+    Material material;
+    void OnEnable()
+    {
+        material = new Material(Shader.Find("Hidden/Internal-Colored"));
+    }
+
+    public override void OnInspectorGUI ()
+    {
+        var spectrum = target as AudioSpectrum;
+
+        // Component properties.
+        spectrum.numberOfSamples = EditorGUILayout.IntPopup ("Number of samples", spectrum.numberOfSamples, sampleOptionStrings, sampleOptions);
+        spectrum.bandType = (AudioSpectrum.BandType)EditorGUILayout.IntPopup ("Band type", (int)spectrum.bandType, bandOptionStrings, bandOptions);
+        spectrum.fallSpeed = EditorGUILayout.Slider ("Fall speed", spectrum.fallSpeed, 0.01f, 0.5f);
+        spectrum.sensibility = EditorGUILayout.Slider ("Sensibility", spectrum.sensibility, 1.0f, 20.0f);
+
+        // Shows the spectrum curve.
+        Rect position = GUILayoutUtility.GetRect(10, 10000, 200, 200);
+        if (Event.current.type == EventType.Repaint)
+        {
+            GUI.BeginClip(position);
+            material.SetPass(0);
+
+            var levels = spectrum.Levels;
+            var means = spectrum.MeanLevels;
+            var peaks = spectrum.PeakLevels;
+
+            var width = position.width;
+            var height = position.height;
+            var step = position.width / levels.Length;
+
+            GL.Begin(GL.LINES);
+            for (var i = 0; i < levels.Length; i++)
+            {
+                GL.Color(Color.green * new Color(1, 1, 1, 0.5f));
+                GL.Vertex3(i * step, levels[i] * height, 0);
+                GL.Vertex3((i+1) * step, levels[i] * height, 0);
+
+                GL.Color(Color.blue * new Color(1, 1, 1, 0.5f));
+                GL.Vertex3(i * step, means[i] * height, 0);
+                GL.Vertex3((i + 1) * step, means[i] * height, 0);
+
+                GL.Color(Color.red * new Color(1, 1, 1, 0.5f));
+                GL.Vertex3(i * step, peaks[i] * height, 0);
+                GL.Vertex3((i + 1) * step, peaks[i] * height, 0);
+
+                var ratio = levels[i] / (peaks[i]+0.01f);
+                GL.Color(Color.black * new Color(1,1,1,0.2f));
+                GL.Vertex3(i * step, ratio * height, 0);
+                GL.Vertex3((i + 1) * step, ratio * height, 0);
+            }
+            GL.End();
+
+            GUI.EndClip();
+        }
+
+        // Update frequently while it's playing.
+        if (EditorApplication.isPlaying) {
+            EditorUtility.SetDirty (target);
+        }
+    }
+}
